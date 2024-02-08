f14a30f22c5823afedfa03c794ce23527830c3d9
Xyves <xodrixx@protonmail.com>
2024-02-08 21:22:32 +0100

    Edit coordinates in ship class

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,10 +1,10 @@
 export default class Ship {
-  constructor(length, coordinates, name, hits = 0, sunk = false) {
+  constructor(length, name, coordinates, hits = 0, sunk = false) {
     this.name = name;
     this.length = length;
     this.hits = hits;
     this.sunk = sunk;
-    this.coordinates = [...Array(length).keys()].map((i) => coordinates + i);
+    this.coordinates = [];
   }
   hit() {
     this.hits++;
