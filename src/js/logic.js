e493ee204dfa5b1735ebf08dc07e4d31f4bcf292
Xyves <xodrixx@protonmail.com>
2024-02-13 21:46:13 +0100

    Add generating direction function

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,19 +1,55 @@
 function generateLocation() {
-  return Math.floor(Math.random() * 10 + 1);
+  return Math.floor(Math.random() * 100);
 }
-function calculateIndex(x, y) {
-  const maxValue = 10;
-  const combinationPerRow = 11;
-  const totalCombination = 100;
+function generateDirection() {
+  let direction = Math.floor(Math.random() * 2);
+  if (direction === 0) {
+    console.log("Vertical");
+    return "vertical";
+  } else {
+    console.log("horizontal");
+    return "horizontal";
+  }
+}
+function checkDuplicates(board, generatedLocations) {
+  for (let generatedLocation of generatedLocations) {
+    for (let i = 0; i < board.length; i++) {
+      const ship = board[i];
+      if (ship.coordinates.includes(generatedLocation)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function exceedsHorizontalBoardEdge(locations, length) {
+  const boardEdge = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+  for (location of locations) {
+    for (let i = location; i < location + length; i++) {
+      if (boardEdge.includes(i)) {
+        return true;
+      }
+    }
+  }
 
-  if (x < 0 || x > maxValue || y < 0 || y >= combinationPerRow) {
-    throw new Error("invalid x and y");
-  }
-  let index = x * combinationPerRow + y;
-  if (index >= totalCombination) {
-    index -= totalCombination;
-  }
-  console.log(index);
-  return index;
+  return false;
 }
-export { generateLocation, calculateIndex };
+function exceedsVerticalBoardEdge(locations, length) {
+  const boardEdge = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
+  for (location of locations) {
+    for (let i = location; i < location + length; i++) {
+      if (boardEdge.includes(i)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export {
+  generateLocation,
+  checkDuplicates,
+  exceedsHorizontalBoardEdge,
+  exceedsVerticalBoardEdge,
+  generateDirection,
+};
