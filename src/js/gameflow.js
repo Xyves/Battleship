f157841378d01e2e882938621078bd5bc24aadc3
Xyves <xodrixx@protonmail.com>
2024-02-29 21:48:41 +0100

    Implement taking turns between user and computer

--- src/js/gameflow.js
+++ src/js/gameflow.js
@@ -3,7 +3,8 @@
 import GameBoard from "./gameboard.js";
 import Player from "./player.js";
 import Ship from "./ship.js";
-import { styleBoard } from "./render.js";
+import { generateLocation } from "./logic.js";
+import { styleBoard, handleDivClick } from "./render.js";
 const startButton = document.querySelector("#startButton");
 export default function startGame() {
   startButton.addEventListener("click", () => {
@@ -13,18 +14,24 @@ export default function startGame() {
   });
 }
 function runGame() {
-  // CreatePlayers(); //*2*
-  const Player1 = new Player();
-  const Player2 = new Player();
+  // Create players and boards
   const mainBoard = new GameBoard();
   const enemyBoard = new GameBoard();
-  // createShips();
-  /*await*/ placeAllShips(mainBoard, createShips(), "#gameBoard1");
-  /*await*/ placeAllShips(enemyBoard, createShips(), "#gameBoard2");
-  // /* await*/ move();
-  console.log(mainBoard);
+  const Player1 = new Player(mainBoard);
+  const Player2 = new Player(enemyBoard);
+  placeAllShips(mainBoard, createShips(), "#gameBoard1");
+  placeAllShips(enemyBoard, createShips(), "#gameBoard2");
+  const enemyBoardDiv = document.querySelector("#gameBoard2");
+  enemyBoardDiv.addEventListener("click", handleDivClick);
+  takeTurns();
+  async function takeTurns() {
+    while (!mainBoard.isGameOver() && !enemyBoard.isGameOver()) {
+      await move(null, Player1, enemyBoard);
+      if (mainBoard.isGameOver() || enemyBoard.isGameOver()) break;
+      await move(null, Player2, mainBoard);
+    }
+  }
 }
-
 function updateDOM() {}
 function createShips() {
   const Carrier = new Ship(5, "Carrier");
@@ -43,7 +50,20 @@ function placeAllShips(board, shipsArray, boardId) {
     } else {
       styleBoard(ship, boardId);
     }
-    // console.log(board);
   }
-  // console.log(board);
+}
+async function move(clickedDataId, player, board) {
+  console.log("Hey");
+  if (player == "Player1") {
+    if (clickedDataId) {
+      // Check if a valid div is clicked
+      player.hit(clickedDataId, board);
+    } else {
+      console.log("Invalid click");
+    }
+  } else {
+    const cords = generateLocation();
+    player.hit(cords, board);
+  }
+  console.log("x");
 }
